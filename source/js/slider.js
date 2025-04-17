1c871209c6355c55f1d88b8d2788aa808ef97927
Grygorii <nakan88@gmail.com>
2025-04-17 17:06:53 +0200

    update styles, add fixes according to the feedback

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,20 +1,20 @@
 const swiper = new Swiper(".swiper", {
-    // Optional parameters
-    autoplay: {
-        delay: 5000,
-    },
-    speed: 400,
-    loop: true,
-    allowTouchMove: true,
+  // Optional parameters
+  autoplay: {
+    delay: 5000,
+  },
+  speed: 400,
+  loop: true,
+  allowTouchMove: true,
+  clickable: true,
+  centeredSlides: true,
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
     clickable: true,
-
-    // If we need pagination
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-})
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
